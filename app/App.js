81acba289ba704a378a80c90fad439e5b53c8bf3
Michał Błotniak <miblotniak@gmail.com>
2018-05-17 22:20:15 +0200

    Disable keyboard handling (not working)

--- app/App.js
+++ app/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
-import { StackNavigator, TabNavigator } from 'react-navigation';
+import { createStackNavigator, createTabNavigator } from 'react-navigation';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 import { ROUTES, INITIAL_ROUTE } from './config/routes';
@@ -41,21 +41,27 @@ const styles = StyleSheet.create({
 
 // TODO: Handle hamburger touches, see: https://github.com/react-navigation/react-navigation/issues/1539#issuecomment-301870732
 // TODO: Refactor below!
-export default StackNavigator({
-  Root: {
-    screen: TabNavigator(ROUTES, {
-      tabBarOptions: { style: styles.tabs, indicatorStyle: styles.indicator },
-      initialRouteName: INITIAL_ROUTE,
-    }),
-    navigationOptions: () => ({
-      headerStyle: styles.header,
-      headerTitle: (
-        <View style={styles.headerTitle}>
-          <Image source={logo} style={styles.logo} resizeMode="contain" />
-          <Text style={styles.headerText}>Ceburilo</Text>
-        </View>
-      ),
-      headerLeft: <Icon name="bars" size={20} style={styles.hamburger} />,
-    }),
+export default createStackNavigator(
+  {
+    Root: {
+      screen: createTabNavigator(ROUTES, {
+        tabBarOptions: { style: styles.tabs, indicatorStyle: styles.indicator },
+        disableKeyboardHandling: true,
+        initialRouteName: INITIAL_ROUTE,
+      }),
+      navigationOptions: () => ({
+        headerStyle: styles.header,
+        headerTitle: (
+          <View style={styles.headerTitle}>
+            <Image source={logo} style={styles.logo} resizeMode="contain" />
+            <Text style={styles.headerText}>Ceburilo</Text>
+          </View>
+        ),
+        headerLeft: <Icon name="bars" size={20} style={styles.hamburger} />,
+      }),
+    },
   },
-});
+  {
+    disableKeyboardHandling: true,
+  },
+);
